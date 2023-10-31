9f3bbd7ca43aeebca274665dee0d6ebf61e53b47
misprachi023 <prachimishra23123@gmail.com>
2023-10-31 12:42:39 +0530

    update timer

--- Timer/src/Timer.jsx
+++ Timer/src/Timer.jsx
@@ -1,35 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
+function fixtimeString(num) {
+  return num <= 9 ? `0${num}` : `${num}`;
+}
+
+const formatTime = (timeInSecs) => {
+  const seconds = timeInSecs % 60;
+  const minutes = Math.floor(timeInSecs / 60) % 60;
+  const hours = Math.floor(timeInSecs / 3600);
+  return `${fixtimeString(hours)}:${fixtimeString(minutes)}:${fixtimeString(
+    seconds
+  )}`;
+};
 
 function Timer() {
-  const [seconds, setSeconds] = useState(0);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setSeconds((count) => count + 1);
+      setCount((precount) => precount + 1);
     }, 1000);
 
     return () => {
-      clearInterval(intervalId); 
+      clearInterval(intervalId);
     };
   }, []);
 
   return (
     <div>
-      <p>Timer : {seconds} seconds</p>
+      {/* <p>Timer : {count} count</p> */}
+      <h6>{formatTime(count)} </h6>
     </div>
   );
 }
 
-
 function App() {
   const [showTimer, setShowTimer] = useState(true);
-  
+
   return (
     <div>
       <h1>Timer App</h1>
+      
       <button onClick={() => setShowTimer(!showTimer)}>
-        {showTimer ? 'Hide Timer' : 'Show Timer'}
+        {showTimer ? "Hide Timer" : "Show Timer"}
       </button>
       {showTimer && <Timer />}
     </div>
